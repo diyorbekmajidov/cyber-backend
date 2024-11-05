af4f18c07a098806487dbf38f960514f2c1673d8
Diyorbek <majidovdiyorbek741@gmail.com>
2024-11-05 12:39:21 +0500

    static files

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$()o3=pa1gee&3!pnjuhp!h+2!49fd=*3t-$go$t664jr(0aug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['kattakurgonst.uz']
